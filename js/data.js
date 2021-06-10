f3028a33820186a1bb103b32b09f1fbfa444b5c3
MarijusIL <78425010+MarijusIL@users.noreply.github.com>
2021-06-10 10:29:02 +0300

    kintamasis su men pavadinimais

--- js/data.js
+++ js/data.js
@@ -12,3 +12,18 @@ const account = [
     { month: 8, income: 450, expense: 120 },
     { month: 12, expense: 200 }
 ];
+
+const monthNames = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+};
