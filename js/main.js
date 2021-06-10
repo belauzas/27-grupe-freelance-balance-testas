bc315f79ad93c450e40be0b5397a12ccd129818f
gretane <greta.sufinskaite@gmail.com>
2021-06-10 11:45:18 +0300

    Import/export js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { account } from './data.js';
+
 const menesiai = [
     'Sausis',
     'Vasaris',
@@ -11,4 +13,12 @@ const menesiai = [
     'Spalis',
     'Lapkritis',
     'Gruodis'
-]
+];
+
+/* 4.
+1. Reikia pasimokyti JS.
+
+ 2. 1)Naudoti cikla for
+    3)Priskirti menesiu pavadinimus pagal menesio numeri
+    4)Iterpti viska i HTML faila su JS surenderinus
+ */
