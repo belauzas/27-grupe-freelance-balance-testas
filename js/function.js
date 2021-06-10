e31fa64eddd8deb7ec1c6f17097ac060f3840f21
Igoris Ivanovas <igoris.x@inbox.lt>
2021-06-10 12:06:38 +0300

    a

--- js/function.js
+++ js/function.js
@@ -17,29 +17,30 @@ class function1 {
 
     render() {
         const itemCount = this.data.length;
-        const monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        const monthName = ['sausis', 'vasaris', 'kovas', 'balandis', 'geguze', 'birzelis', 'liepa', 'rugpjutis', 'rugsejis', 'spalis', 'lapkritis', 'gruodis']
         let HTML = ''
+        let allYearIncome = 0;
+        let allYearExpense = 0;
+
 
 
         for (let i = 0; i < itemCount; i++) {
-
-
-
 
             HTML += `<div class="table-row">
                                     <div class="cell">${this.data[i].month}</div>
                                     <div class="cell">${monthName[this.data[i].month - 1]}</div>
                                     <div class="cell">${this.data[i].income} Eur</div>
                                     <div class="cell">${this.data[i].expense} Eur</div>
-                                    <div class="cell">150.00 Eur</div>
+                                    <div class="cell">${this.data[i].income - this.data[i].expense} Eur</div>
                                 </div>`
+            allYearIncome += this.data[i].income
+            allYearExpense += this.data[i].expense
         }
 
+        let leftMoney = allYearIncome - allYearExpense;
+
+
         this.DOM.innerHTML = HTML;
-
-
-
-
     }
 
 
