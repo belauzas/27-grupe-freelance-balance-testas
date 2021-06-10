f66a9545e555b97a880b3a9fc042844f382f566f
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-06-10 10:36:40 +0300

    menesiai prideti

--- js/data.js
+++ js/data.js
@@ -1,14 +1,70 @@
 const account = [
-    { month: 10, income: 150 },
-    { month: 1, income: 200, expense: 50 },
-    { month: 3, income: 450, expense: 200 },
-    { month: 7, expense: 50 },
-    { month: 2, income: 50 },
-    { month: 5, income: 150, expense: 50 },
-    { month: 9, income: 700, expense: 400 },
-    { month: 4, income: 10, expense: 300 },
-    { month: 6, expense: 50 },
-    { month: 11, income: 150, expense: 30 },
-    { month: 8, income: 450, expense: 120 },
-    { month: 12, expense: 200 }
+    {
+        month: 10,
+        name: 'spalis',
+        income: 150
+    },
+    {
+        month: 1,
+        name: 'sausis',
+        income: 200,
+        expense: 50
+    },
+    {
+        month: 3,
+        name: 'kovas',
+        income: 450,
+        expense: 200
+    },
+    {
+        month: 7,
+        name: 'kovas',
+        expense: 50
+    },
+    {
+        month: 2,
+        name: 'vasaris',
+        income: 50
+    },
+    {
+        month: 5,
+        name: 'geguze',
+        income: 150,
+        expense: 50
+    },
+    {
+        month: 9,
+        name: 'rugsejis',
+        income: 700,
+        expense: 400
+    },
+    {
+        month: 4,
+        name: 'balandis',
+        income: 10,
+        expense: 300
+    },
+    {
+        month: 6,
+        name: 'liepa',
+        expense: 50
+    },
+    {
+        month: 11,
+        name: 'lapkritis',
+        income: 150,
+        expense: 30
+    },
+    {
+        month: 8,
+        name: 'rugpjutis',
+        income: 450,
+        expense: 120
+    },
+    {
+        month: 12,
+        name: 'gruodis',
+        expense: 200
+    }
 ];
+export { account }
