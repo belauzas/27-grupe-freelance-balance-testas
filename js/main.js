bb793335b1451a2341383af41954c8e6e650fe8a
gretane <greta.sufinskaite@gmail.com>
2021-06-10 10:42:39 +0300

    Kintamasis menesiai

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const menesiai = [
+    'Sausis',
+    'Vasaris',
+    'Kovas',
+    'Balandis',
+    'Geguze',
+    'Birzelis',
+    'Liepa',
+    'Rugpjutis',
+    'Rugsejis',
+    'Spalis',
+    'Lapkritis',
+    'Gruodis'
+]
